ff1a584f8128496e78f82e56df6187388de0a823
erifranck <erifranckjose@gmail.com>
2018-02-17 13:21:59 -0500

    change routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import {HashRouter, Route} from 'react-router-dom'
-import App from './App'
+import {HashRouter, Switch} from 'react-router-dom'
+import AppRoute from './components/AppRouter'
+import Layout from './components/Layout/Layout'
+import Home from './pages/Home/Home'
+import About from './pages/About'
+import Portfolio from './pages/Portfolio'
 
 const Routes = () => (
   <HashRouter>
-    <Route path='/' component={App} />
+    <Switch>
+        <AppRoute path='/' Component={Home} Layout={Layout} exact />
+        <AppRoute path='/about' Component={About} Layout={Layout} />
+        <AppRoute path='/portfolio' Component={Portfolio} Layout={Layout} />
+    </Switch>
   </HashRouter>
 )
 
